97f98a088753fad0e655b76f06e3579939c6911b
kazvee <kayvee123@gmail.com>
2023-09-08 15:14:19 -0600

    Implement favourite icon component functionality

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,13 +1,15 @@
-import React, { useCallback, useState } from 'react';
-
+import React from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
-function PhotoFavButton() {
+function PhotoFavButton(props) {
+  
+  const { isFavourite, toggleFavourite } = props;
+
   return (
-    <div className="photo-list__fav-icon">
-      <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+    <div className='photo-list__fav-icon' onClick={toggleFavourite}>
+      <div className='photo-list__fav-icon-svg'>
+        <FavIcon selected={isFavourite} />
       </div>
     </div>
   );
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/PhotoListItem.scss';
+import PhotoFavButton from './PhotoFavButton';
 
 const PhotoListItem = (props) => {
-  const { username, imageSource, location, profile } = props;
+
+  const { username, imageSource, location, profile, isFavourite } = props;
+  const [isFave, setIsFave] = useState(isFavourite);
+
+  const toggleFavourite = () => {
+    setIsFave(!isFave);
+    // TODO: Remove console logs
+    if (isFave) {
+      console.log("Unfavourited! ☹️");
+    } else {
+      console.log("Favourited! 😍");
+    }
+  };
+
   return (
     <div className='photo-list__item'>
+      <PhotoFavButton isFavourite={isFave} toggleFavourite={toggleFavourite} />
       <img className='photo-list__image' src={imageSource} alt={`Photo of ${location.city}, ${location.country}`} />
       <div className='photo-list__user-details'>
         <img className='photo-list__user-profile' src={profile} alt={`Profile photo of ${username}`} />
