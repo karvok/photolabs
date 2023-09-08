4e5aa010556e372526152da1d29fde135a04316d
kazvee <kayvee123@gmail.com>
2023-09-08 13:27:32 -0600

    Refactor App to render 3 PhotoListItem components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,9 +15,19 @@ const sampleDataForPhotoListItem = {
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+
+  // Create array of the same data 3 times
+  const photos = new Array(3).fill(sampleDataForPhotoListItem);
+
   return (
-    <div className="App">
-      <PhotoListItem key={sampleDataForPhotoListItem.id} {...sampleDataForPhotoListItem}/>
+    <div className='App'>
+
+      {/* Use map() to render each PhotoListItem */}
+      {photos.map((photo, index) => (
+
+        // Provide unique key for each photo by combining the photo's id and its index in the array
+        <PhotoListItem key={photo.id + index} {...photo} />
+      ))}
     </div>
   );
 };
