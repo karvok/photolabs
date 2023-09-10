6647674ea592c21fa40285f793d508ed32922fdc
kazvee <kayvee123@gmail.com>
2023-09-10 14:59:35 -0600

    Build TopNavigationBar component which renders topics and heart icon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import TopNavigationBar from './components/TopNavigationBar';
 import PhotoList from 'components/PhotoList';
-import TopicList from 'components/TopicList';
 import './App.scss';
 
 const App = () => {
   return (
     <div className='App'>
-      <TopicList />
+      <TopNavigationBar />
       <PhotoList />
     </div>
   );
