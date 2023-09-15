261c11d9bb8f7ae142981842afabe904e291aa47
kazvee <kayvee123@gmail.com>
2023-09-14 20:34:16 -0600

    Refactor: Handle useApplicationData state management with useReducer

--- frontend/src/hooks/useApplicationData.jsx
+++ frontend/src/hooks/useApplicationData.jsx
@@ -1,25 +1,64 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
+
+export const ACTIONS = {
+  FAV_PHOTO_TOGGLED: 'FAV_PHOTO_TOGGLED',
+  DISPLAY_PHOTO_DETAILS: 'DISPLAY_PHOTO_DETAILS',
+  CLOSE_PHOTO_DETAILS: 'CLOSE_PHOTO_DETAILS',
+};
+
+const initialState = {
+  displayModal: false,
+  favourites: [],
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FAV_PHOTO_TOGGLED':
+      const { selectedPhotoId } = action.payload;
+      if (state.favourites.includes(selectedPhotoId)) {
+        return {
+          ...state,
+          favourites: state.favourites.filter(
+            (alreadyFavoritedPhotoId) =>
+              alreadyFavoritedPhotoId !== selectedPhotoId
+          ),
+        };
+      }
+      return { ...state, favourites: [...state.favourites, selectedPhotoId] };
+
+    case 'DISPLAY_PHOTO_DETAILS':
+      const { photo } = action.payload;
+      return {
+        ...state,
+        displayModal: photo,
+      };
+
+    case 'CLOSE_PHOTO_DETAILS':
+      const { value } = action.payload;
+      return {
+        ...state,
+        displayModal: value,
+      };
+
+    default:
+      throw new Error(`
+        Tried to reduce with unsupported action type: ${action.type}`);
+  }
+};
 
 const useApplicationData = () => {
-  const [state, setState] = useState({
-    displayModal: false,
-    favourites: [],
-  });
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const updateToFavPhotoIds = (selectedPhotoId) => {
+    dispatch({ type: 'FAV_PHOTO_TOGGLED', payload: { selectedPhotoId } });
+  };
 
   const setPhotoSelected = (photo) => {
-    setState({ ...state, displayModal: photo });
+    dispatch({ type: 'DISPLAY_PHOTO_DETAILS', payload: { photo } });
   };
 
   const onClosePhotoDetailsModal = (value) => {
-    setState({ ...state, displayModal: value });
-  };
-
-  const updateToFavPhotoIds = (photoId) => {
-    if (state.favourites.includes(photoId)) {
-      setState({ ...state, favourites: [...state.favourites].filter((alreadyFavoritedPhotoId) => photoId !== alreadyFavoritedPhotoId) });
-      return;
-    }
-    setState({ ...state, favourites: [...state.favourites, photoId] });
+    dispatch({ type: 'CLOSE_PHOTO_DETAILS', payload: { value } });
   };
 
   return {
