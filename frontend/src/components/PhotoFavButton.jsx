0075ee3a1653be369097fd16902edf8766ef43ce
kazvee <kayvee123@gmail.com>
2023-09-16 12:10:55 -0600

    Refactor PhotoFavButton to use arrow function

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
-function PhotoFavButton({ isFavourite, updateToFavPhotoIds }) {
+const PhotoFavButton = ({ isFavourite, updateToFavPhotoIds }) => {
   return (
     <div className='photo-list__fav-icon' onClick={updateToFavPhotoIds}>
       <div className='photo-list__fav-icon-svg'>
@@ -10,6 +10,6 @@ function PhotoFavButton({ isFavourite, updateToFavPhotoIds }) {
       </div>
     </div>
   );
-}
+};
 
 export default PhotoFavButton;
