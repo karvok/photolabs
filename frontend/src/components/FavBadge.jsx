7340f5a670990b6a2a858d15eb8c8df5082ed2ae
kazvee <kayvee123@gmail.com>
2023-09-16 14:05:39 -0600

    Add click handler to FavBadge to display if favourited photos exist

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -3,8 +3,12 @@ import FavIcon from './FavIcon';
 import '../styles/FavBadge.scss';
 
 const FavBadge = ({ isFavPhotoExist }) => {
+  const handleFavouritesClick = () => {
+    console.log(`😍 Clicked! Existing favourited photos: ${isFavPhotoExist}`);
+  };
+
   return (
-    <div className='fav-badge'>
+    <div className='fav-badge' onClick={handleFavouritesClick}>
       <FavIcon displayAlert={!!isFavPhotoExist} selected={true} />
     </div>
   );
